95c2babb29e5c9093de3a453ecc88ebabcc9bfd6
RedJocker85 <mauricio.guaruja@gmail.com>
2021-04-21 21:17:56 -0300

    StreamingConfig created

---
+++ checkout/src/main/java/org/diotutorial/ecommerce/checkout/config/StreamingConfig.java
@@ -0,0 +1,12 @@
+package org.diotutorial.ecommerce.checkout.config;
+
+import org.diotutorial.ecommerce.checkout.streaming.CheckoutCreatedSource;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBinding(value = {
+        CheckoutCreatedSource.class,
+})
+public class StreamingConfig {
+}
